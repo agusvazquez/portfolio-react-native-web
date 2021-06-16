2aa8436904bbf221e21f2c5eb028235b4b62c95d
Agustin Alejandro Vazquez Ballan <aguss.vazquez@gmail.com>
2021-06-16 14:48:04 -0300

    Finished Deck animation component

--- components/Deck.tsx
+++ components/Deck.tsx
@@ -1,27 +1,68 @@
 import React from "react";
 import {
   StyleSheet,
+  View,
   Animated,
   PanResponder,
   Dimensions,
   GestureResponderEvent,
   PanResponderGestureState,
+  Platform,
+  UIManager,
+  LayoutAnimation,
 } from "react-native";
-import { Text } from "react-native-elements";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
-const Deck = () => {
+interface Props<T> {
+  data: T[];
+  renderNoMoreCards: () => React.ReactNode;
+  renderCard: (item: T) => React.ReactNode;
+  onSwipeLeft?: (item: T) => void;
+  onSwipeRight?: (item: T) => void;
+}
+
+if (
+  Platform.OS === "android" &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+const Deck = ({
+  data,
+  renderNoMoreCards,
+  renderCard,
+  onSwipeLeft = () => {},
+  onSwipeRight = () => {},
+}: Props<any>) => {
   const position = React.useRef(new Animated.ValueXY()).current;
+
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
+
+  React.useEffect(() => {
+    setCurrentIndex(0);
+  }, [data]);
+
+  const onSwipeComplete = (x: number) => {
+    const item = data[currentIndex];
+
+    x > 0 ? onSwipeRight(item) : onSwipeLeft(item);
+    position.setValue({ x: 0, y: 0 });
+
+    const newIndex = currentIndex + 1;
+    setCurrentIndex(newIndex);
+    LayoutAnimation.spring();
+  };
 
   const forceSwipe = (x: number) => {
     Animated.timing(position, {
       useNativeDriver: false,
       toValue: { x, y: 0 },
       duration: SWIPE_OUT_DURATION,
-    }).start();
+    }).start(() => onSwipeComplete(x));
   };
 
   const forceSwipeRight = () => {
@@ -54,15 +95,15 @@ const Deck = () => {
     }
   };
 
-  const panResponder = React.useRef(
-    PanResponder.create({
+  const panResponder = React.useMemo(() => {
+    return PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
       onPanResponderRelease,
-    })
-  ).current;
+    });
+  }, [currentIndex]);
 
   const getCardStyle = () => {
     const rotate = position.x.interpolate({
@@ -70,26 +111,54 @@ const Deck = () => {
       outputRange: ["-50deg", "0deg", "50deg"],
     });
 
-    return { ...position.getLayout(), ...styles.box, transform: [{ rotate }] };
+    return { ...position.getLayout(), transform: [{ rotate }] };
   };
 
-  return (
-    <Animated.View {...panResponder.panHandlers} style={getCardStyle()}>
-      <Text style={styles.text}>This is my Animated View</Text>
-    </Animated.View>
-  );
+  const renderCards = () => {
+    if (currentIndex >= data.length) {
+      return renderNoMoreCards();
+    }
+
+    return data
+      .map((item, elementIndex) => {
+        if (elementIndex < currentIndex) {
+          return null;
+        }
+
+        if (elementIndex === currentIndex) {
+          return (
+            <Animated.View
+              key={item.id}
+              style={[getCardStyle(), styles.cardStyle, { zIndex: 10 }]}
+              {...panResponder.panHandlers}
+            >
+              {renderCard(item)}
+            </Animated.View>
+          );
+        }
+
+        return (
+          <Animated.View
+            key={item.id}
+            style={[
+              styles.cardStyle,
+              { top: 10 * (elementIndex - currentIndex), zIndex: 5 },
+            ]}
+          >
+            {renderCard(item)}
+          </Animated.View>
+        );
+      })
+      .reverse();
+  };
+
+  return <View>{renderCards()}</View>;
 };
 
 const styles = StyleSheet.create({
-  text: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  box: {
-    height: 150,
-    width: 150,
-    backgroundColor: "blue",
-    borderRadius: 5,
+  cardStyle: {
+    position: "absolute",
+    width: SCREEN_WIDTH,
   },
 });
 
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,11 +1,80 @@
-import * as React from "react";
-import { StyleSheet, View } from "react-native";
+import React from "react";
+import { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Card, Button } from "react-native-elements";
 import Deck from "../components/Deck";
 
+const DATA = [
+  {
+    id: 1,
+    title: "Card 1",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+  },
+  {
+    id: 2,
+    title: "Card 2",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg",
+  },
+  {
+    id: 3,
+    title: "Card 3",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+  },
+  {
+    id: 4,
+    title: "Card 4",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg",
+  },
+  {
+    id: 5,
+    title: "Card 5",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+  },
+];
+
+type ItemType = {
+  id: number;
+  title: string;
+  uri: string;
+};
+
 export default function TabOneScreen() {
+  const [data, setData] = useState<ItemType[]>(DATA);
+
+  const fetchMorePressed = () => {
+    setData([...DATA]);
+  };
+
+  const renderCard = (item: ItemType) => {
+    return (
+      <Card>
+        <Card.Image source={{ uri: item.uri }} />
+        <Card.Title>{item.title}</Card.Title>
+        <Card.Divider />
+
+        <Button title="View Now!" />
+      </Card>
+    );
+  };
+
+  const renderNoMoreCards = () => {
+    return (
+      <Card>
+        <Card.Title>All Done</Card.Title>
+        <Card.Divider />
+
+        <Button title="Fetch More" onPress={fetchMorePressed} />
+      </Card>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Deck />
+      <Deck
+        data={data}
+        renderCard={renderCard}
+        renderNoMoreCards={renderNoMoreCards}
+      />
     </View>
   );
 }
@@ -13,7 +82,5 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
